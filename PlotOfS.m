3a1da3c460d41f58edf414534fbbf6fe78ad7388
Riccardo Musso <musso.riccardo2001@gmail.com>
2024-03-04 17:43:31 +0100

    Lot of changes

--- PlotOfS.m
+++ PlotOfS.m
@@ -7,7 +7,7 @@ c = 299792458;
 
 % Sizes of the apertures
 a1 = 1.5e-2;  % 1.5cm
-a2 = pi*1e-2; % 3.14 cm
+a2 = 2*pi*1e-2; % 3.14 cm
 a3 = 1e-2;    % 1cm
 
 % Location of apertures
@@ -16,14 +16,17 @@ xoff2 = 0;         % 0 cm
 xoff3 = 5e-3;      % 0.5 cm
 
 % Length of the middle waveguide
-delta_z = 3e-2;     % 3 cm
+delta_z = 3e-2;     % 30 cm
 
-w = 0:10e6:100e9;
+w = (14.99e9:10e6:50e9)*2*pi;
+f = w /2/pi;
 k = w/c;
 
 n_t1 = pi/a1;
 n_t2 = pi/a2;
 n_t3 = pi/a3;
+
+fprintf("Resonance frequency a2: %d", (c/2*sqrt((n_t2/pi)^2 + (1/delta_z)^2)));
 
 S11_L = zeros(length(w), 1);
 S12_L = zeros(length(w), 1);
@@ -38,13 +41,13 @@ S12_LR = zeros(length(w), 1);
 S21_LR = zeros(length(w), 1);
 S22_LR = zeros(length(w), 1);
 
-for i = 1:length(w)    
+for i = 1:length(w)
     n_z1 = nzfromnt(n_t1, k(i));
     n_z2 = nzfromnt(n_t2, k(i));
     n_z3 = nzfromnt(n_t3, k(i));
 
     [S11_L(i), S12_L(i), S21_L(i), S22_L(i)] = aperture_S(n_t1, n_t2, n_z1, n_z2, 1, 1, k(i), k(i), xoff1, a1, a2);
-    P = exp(-1j*n_z2*delta_z);
+    P = exp(-1i*n_z2*delta_z);
     [S22_R(i), S12_R(i), S21_R(i), S11_R(i)] = aperture_S(n_t3, n_t2, n_z3, n_z2, 1, 1, k(i), k(i), xoff3, a3, a2);
     [S11_PR, S12_PR, S21_PR, S22_PR] = combineLR(0, P, P, 0, S11_R(i), S12_R(i), S21_R(i), S22_R(i));
     [S11_LR(i), S12_LR(i), S21_LR(i), S22_LR(i)] = combineLR(S11_L(i), S12_L(i), S21_L(i), S22_L(i), S11_PR, S12_PR, S21_PR, S22_PR);
@@ -53,32 +56,32 @@ end
 figure(1)
 tiledlayout(3,2);
 nexttile;
-plot(w, real(S11_L), "b");
+plot(f, abs(S11_L), "b");
 title("S11L");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S12_L), "b");
+plot(f, abs(S12_L), "b");
 title("S12L");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S21_L));
+plot(f, abs(S21_L));
 title("S21L");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S22_L));
+plot(f, abs(S22_L));
 title("S22L");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S11_L).^2 + abs(S21_L).^2);
-title("S11^2 + S21^2");
+plot(f, abs(S11_L).^2 + abs(S12_L).^2);
+title("S11^2 + S12^2");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S22_L).^2 + abs(S21_L).^2);
+plot(f, abs(S22_L).^2 + abs(S21_L).^2);
 title("S22^2 + S21^2");
 xlabel("Frequency [Hz]");
 
@@ -86,71 +89,71 @@ xlabel("Frequency [Hz]");
 figure(2)
 tiledlayout(3,2);
 nexttile;
-plot(w, real(S11_R), "b");
+plot(f, real(S11_R), "b");
 title("S11R");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S12_R), "b");
+plot(f, real(S12_R), "b");
 title("S12R");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S21_R));
+plot(f, real(S21_R));
 title("S21R");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, real(S22_R));
+plot(f, real(S22_R));
 title("S22R");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S11_R).^2 + abs(S21_R).^2);
+plot(f, abs(S11_R).^2 + abs(S12_R).^2);
 title("S11^2 + S21^2");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S22_R).^2 + abs(S21_R).^2);
+plot(f, abs(S22_R).^2 + abs(S21_R).^2);
 title("S22^2 + S21^2");
 xlabel("Frequency [Hz]");
 
 figure(3);
 tiledlayout(3,2);
 nexttile;
-plot(w, abs(S11_LR), "b");
+plot(f, abs(S11_LR), "b");
 hold on;
-plot(w, imag(S11_LR), "r");
+plot(f, imag(S11_LR), "r");
 title("S11");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S12_LR), "b");
+plot(f, abs(S12_LR), "b");
 hold on;
-plot(w, imag(S12_LR), "r");
+plot(f, imag(S12_LR), "r");
 title("S12");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S21_LR), "b");
+plot(f, abs(S21_LR), "b");
 hold on;
-plot(w, imag(S21_LR), "r");
+plot(f, imag(S21_LR), "r");
 title("S21");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S22_LR), "b");
+plot(f, abs(S22_LR), "b");
 hold on;
-plot(w, imag(S11_LR), "r");
+plot(f, imag(S11_LR), "r");
 title("S22");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S11_LR).^2 + abs(S21_LR).^2);
+plot(f, abs(S11_LR).^2 + abs(S12_LR).^2);
 title("S11^2 + S21^2");
 xlabel("Frequency [Hz]");
 
 nexttile;
-plot(w, abs(S22_LR).^2 + abs(S21_LR).^2);
+plot(f, abs(S22_LR).^2 + abs(S21_LR).^2);
 title("S22^2 + S21^2");
 xlabel("Frequency [Hz]");
